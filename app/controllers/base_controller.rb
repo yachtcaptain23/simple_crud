661bbe1f20a6d8fb96a5ae461a9799dc55f69fba
Albert W <amwang218@gmail.com>
2023-03-27 11:28:08 -0700

    Add auth

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,16 +1,31 @@
 class BaseController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
   API_AUTH_TOKEN = Rails.application.secrets[:api_auth_token].freeze
 
   before_action :authenticate
+  before_action :check_auth
+
+  class NotAuthorized < StandardError; end
+
+  rescue_from NotAuthorized, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    render(json: {}.to_json, status: 401)
+  end
 
   def authenticate
-    return true if API_AUTH_TOKEN.blank? # && (Rails.env.development? || Rails.env.test?)
-    authenticate_with_http_token do |token, _options|
+    authenticate_or_request_with_http_token do |token, _options|
       # Compare the tokens in a time-constant manner, to mitigate timing attacks.
-      ActiveSupport::SecurityUtils.secure_compare(
+      @authenticated = ActiveSupport::SecurityUtils.secure_compare(
         ::Digest::SHA256.hexdigest(token),
         ::Digest::SHA256.hexdigest(API_AUTH_TOKEN)
       )
     end
+  end
+
+  def check_auth
+    raise NotAuthorized unless @authenticated
   end
 end
